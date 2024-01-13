20270979b74cecb961e8c7b18f87350ed345b478
Elissa Thomas <4015794+et-code-home@users.noreply.github.com>
2024-01-13 12:21:39 +0000

    update preference logic

--- pairings.py
+++ pairings.py
@@ -1,6 +1,12 @@
+import math
+
 AVOID = -1
 NEUTRAL = 0
 PREFERRED = 1
+
+# find the intersection of n sets
+def intersection(first, *others):
+    return set(first).intersection(*others)
 
 def separate_students(preferences, group_size):
     students = list(preferences.keys())
@@ -60,16 +66,37 @@ from preferences import pairing_preferences, project_preferences
 
 group_size = 5
 groups = [[] for _ in range(len(pairing_preferences) // group_size + 1)]
+group_types = [[] for _ in range(len(pairing_preferences) // group_size + 1)]
 placed = []
 
 project_options = 2
 projects = [[] for _ in range(project_options)]
-for i in range(len(projects)):
-    for student in project_preferences:    
-        if project_preferences[student][i] >= 3:
-            projects[i].append(student)
+for student in project_preferences:    
+    if project_preferences[student][0] >= 3:
+        projects[0].append(student)
+    if project_preferences[student][1] >= 3:
+        projects[1].append(student)
+    if project_preferences[student][0] <= 2 and project_preferences[student][1] <= 2:
+        projects[0].append(student)
+        projects[1].append(student)
 
-print(projects)
+
+# convert to sets, find difference (clear preference) and intersection (neutral)
+projects_sets = {}
+projects_sets['1'] = set(projects[0]).difference(set(projects[1]))
+projects_sets['2'] = set(projects[1]).difference(set(projects[0]))
+projects_sets['both'] = set(projects[0]).intersection(set(projects[1]))
+print(projects_sets)
+i = 0
+num = math.ceil(len(projects_sets['1'])//group_size)
+while i < num:
+    group_types[i] = '1'
+    i += 1
+num = math.ceil(len(projects_sets['2'])//group_size)
+while i < num:
+    group_types[i] = '2'
+    i += 1
+
 
 separate_students(pairing_preferences, group_size)
 print(groups)
